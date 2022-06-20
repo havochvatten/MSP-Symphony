3682e968e047c7e65d622e642a29edf2d37a6ca9
Tobias Gerdin <tobias.gerdin@havochvatten.se>
2022-06-20 15:45:15 +0200

    Update build-info for about dialog

--- frontend/src/build-info.ts
+++ frontend/src/build-info.ts
@@ -1,6 +1,6 @@
-// This file is updated by Jenkins and replaced with actual build information
+// This file should be updated by CI system with actual build information
 export default {
-  version: '1.1-SNAPSHOT',
-  buildNumber: 'X',
-  gitSha: '2e50590a1bebfdc4b4066dbe4d393a007606c9fd' // Deeplink to source repo?
+  version: '1.1.0',
+  buildNumber: '<unknown>',
+  gitSha: '<unknown>' // Deeplink to source repo?
 };
