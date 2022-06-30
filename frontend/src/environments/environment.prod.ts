5aba1e23a7edfef997e97b1dd674ab0fbf23a44d
Tobias Gerdin <tobias.gerdin@havochvatten.se>
2022-06-30 13:09:01 +0200

    Request latest baseline in prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   showBaseCalculations: true,
   showIncludeCoastCheckbox: true,
   apiBaseUrl: "/symphony-ws/service",
-  baseline: 'BASELINE2019', // set to some false value to use default
+  baseline: 'BASELINE2019_CM', // set to some false value to use default
   map: {
     center: [14.94, 60.57], // Modified center of Sweden with focus on seas
     maxZoom: 10,
