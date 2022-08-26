3d13493fceb9ffe65759a4c450f5125821c9c4cb
Tobias Gerdin <tobias.gerdin@havochvatten.se>
2022-08-26 17:11:30 +0200

    Set versions for release 1.2.0

--- frontend/src/build-info.ts
+++ frontend/src/build-info.ts
@@ -1,6 +1,6 @@
 // This file should be updated by CI system with actual build information
 export default {
-  version: '1.2.0-SNAPSHOT',
-  buildNumber: '<unknown>',
-  gitSha: '<unknown>' // Deeplink to source repo?
+  version: '1.2.0',
+  buildNumber: '50',
+  gitSha: '2bac856290a36ac007d36631edac81259f5687f5' // Deeplink to source repo?
 };
