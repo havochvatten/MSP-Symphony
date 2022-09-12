7b6d3c31f4753641d633be17cc263c05c0563257
Ann Ahlsten <annahl@hav.havochvatten.se>
2022-09-12 16:35:35 +0200

    Relative difference in totals of scenario comparison
    Pre-review commits

--- frontend/src/app/report/cumulative-effect-etc/cumulative-effect-etc.component.ts
+++ frontend/src/app/report/cumulative-effect-etc/cumulative-effect-etc.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { formatNumber } from '@angular/common';
+import { TranslateService } from "@ngx-translate/core";
 import { NormalizationOptions, NormalizationType } from "@data/calculation/calculation.service";
 import { Report } from "@data/calculation/calculation.interfaces";
 
@@ -14,6 +16,8 @@ export class CumulativeEffectEtcComponent {
   @Input() normalization?: NormalizationOptions;
   @Input() locale = 'en';
 
+  constructor(private translate : TranslateService) {}
+
   type = NormalizationType; // make enum available to template
 
   get areaKm2() {
@@ -22,4 +26,16 @@ export class CumulativeEffectEtcComponent {
 
   typeOf = (obj: any) => typeof obj;
   hasAreaTypes = (matrix: any) => typeof matrix === 'object' &&  Object.keys(matrix.areaTypes).length;
+
+  /* Assumes two reports */
+  relativeDifferencePercentage(prop: string) {
+    const pkey = prop as keyof Report,
+          a = this.reports ? this.reports[0][pkey] as number : 0,
+          b = this.reports ? this.reports[1][pkey] as number : 0,
+          result = ((b - a) / a);
+
+    return (isNaN(result)) ?
+          this.translate.instant('report.cumulative-effect-etc.not-measurable') :
+          formatNumber(result * 100, this.locale, '1.0-2') + "%";
+  }
 }
