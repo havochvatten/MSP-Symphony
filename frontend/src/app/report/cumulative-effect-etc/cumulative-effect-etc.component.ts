68d12026891e13f77d9bc950d023905e64278d37
Ann Ahlsten <annahl@hav.havochvatten.se>
2022-09-13 01:54:37 +0200

    Guard edge case: division by zero

--- frontend/src/app/report/cumulative-effect-etc/cumulative-effect-etc.component.ts
+++ frontend/src/app/report/cumulative-effect-etc/cumulative-effect-etc.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { formatNumber } from '@angular/common';
+import { TranslateService } from "@ngx-translate/core";
 import { NormalizationOptions, NormalizationType } from "@data/calculation/calculation.service";
 import { Report } from "@data/calculation/calculation.interfaces";
 
@@ -14,6 +16,8 @@ export class CumulativeEffectEtcComponent {
   @Input() normalization?: NormalizationOptions;
   @Input() locale = 'en';
 
+  constructor(private translate : TranslateService) {}
+
   type = NormalizationType; // make enum available to template
 
   get areaKm2() {
@@ -27,9 +31,11 @@ export class CumulativeEffectEtcComponent {
   relativeDifferencePercentage(prop: string) {
     const pkey = prop as keyof Report,
           a = this.reports ? this.reports[0][pkey] as number : 0,
-          b = this.reports ? this.reports[1][pkey] as number : 0;
-    return a > 0 ?
-        ((b - a) / a) * 100 :
-        0;
+          b = this.reports ? this.reports[1][pkey] as number : 0,
+          result = ((b - a) / a);
+
+    return (result === NaN) ?
+          this.translate.instant('report.cumulative-effect-etc.not-measurable') :
+          formatNumber(result * 100, this.locale, '1.0-2') + "%";
   }
 }
