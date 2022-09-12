e57a65e585776f4fb494cfda9d1e84cf0c7f9df6
Ann Ahlsten <annahl@hav.havochvatten.se>
2022-09-12 16:35:35 +0200

    Relative difference in totals of scenario comparison

--- frontend/src/app/report/cumulative-effect-etc/cumulative-effect-etc.component.ts
+++ frontend/src/app/report/cumulative-effect-etc/cumulative-effect-etc.component.ts
@@ -22,4 +22,11 @@ export class CumulativeEffectEtcComponent {
 
   typeOf = (obj: any) => typeof obj;
   hasAreaTypes = (matrix: any) => typeof matrix === 'object' &&  Object.keys(matrix.areaTypes).length;
+  relativeDifferencePercentage(prop: string) {
+    const pkey = prop as keyof Report;
+    return this.reports && this.reports.length === 2 && (this.reports[0][pkey] as number) > 0 ?
+        ((this.reports[1][pkey] as number) /
+         (this.reports[0][pkey] as number)) * 100 :
+      0;
+  }
 }
