ec19a9a003b484c40ba46cf414ae7fc41c35597b
Ann Ahlsten <annahl@hav.havochvatten.se>
2022-09-13 14:34:42 +0200

    Handle "numeric" (actual) zero diff

--- frontend/src/app/data/calculation/calculation.util.ts
+++ frontend/src/app/data/calculation/calculation.util.ts
@@ -2,8 +2,9 @@ import { formatNumber } from "@angular/common";
 
 export function relativeDifference(a: any, b: any) {
   const na = a as number,
-        nb = b as number;
-  return (nb - na) / na;
+        nb = b as number,
+        numeric_zero_diff = typeof a === 'number' && typeof b === 'number' && nb === 0 && na === 0;
+  return numeric_zero_diff ? 0 : (nb - na) / na;
 }
 export function formatRelativePercentage(value: number, decimals : number, ersatz : string, locale : string){
   return (value === Infinity || isNaN(value)) ?
