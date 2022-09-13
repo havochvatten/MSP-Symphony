2f91fece78760b1acdbdb8f117732b93d12da877
Ann Ahlsten <annahl@hav.havochvatten.se>
2022-09-13 17:03:06 +0200

    Negative infinity

--- frontend/src/app/data/calculation/calculation.util.ts
+++ frontend/src/app/data/calculation/calculation.util.ts
@@ -7,7 +7,7 @@ export function relativeDifference(a: any, b: any) {
   return numeric_zero_diff ? 0 : (nb - na) / na;
 }
 export function formatRelativePercentage(value: number, decimals : number, ersatz : string, locale : string){
-  return (value === Infinity || isNaN(value)) ?
+  return (Math.abs(value) === Infinity || isNaN(value)) ?
     ersatz :
     (value > 0 ? "+" : "") +  formatNumber(value * 100, locale, '1.0-' + decimals) + "%";
 }
