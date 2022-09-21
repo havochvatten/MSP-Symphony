eb59a7c918827acdafe55b5067584e2f6c1e234d
Ann <ann.ahlsten@havochvatten.se>
2022-09-21 09:15:01 +0200

    100 x value in rel. diff of impact table
    Correcting commit d9424ee

--- frontend/src/app/report/impact-table/impact-table.component.ts
+++ frontend/src/app/report/impact-table/impact-table.component.ts
@@ -19,6 +19,6 @@ export class ImpactTableComponent {
   }
 
   relativeDifferencePercentage(value: number, ersatz: string) {
-    return isNaN(value) ? "" : formatRelativePercentage(value, 2, this.locale, ersatz);
+    return isNaN(value) ? "" : formatRelativePercentage(value / 100, 2, this.locale, ersatz);
   }
 }
