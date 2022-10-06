06b1019bdb2b33b34e10293285d483128ed80a07
Jan Slaninka <jano.slaninka@gmail.com>
2022-10-06 12:56:56 +0200

    changes before updating to v10

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,23 +2,37 @@ import { AuthenticationGuard } from './login/authentication.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainViewComponent } from './map-view/main-view.component';
+import { ComparisonReportComponent } from './report/comparison-report.component';
+import { CalculationReportComponent } from './report/calculation-report.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then(mod => mod.LoginModule)
+    //loadChildren: () => import('./login/login.module').then(mod => mod.LoginModule)
+    component: LoginComponent
   },
   {
     path: 'map', // TODO: rename to main
     component: MainViewComponent,
     canActivate: [AuthenticationGuard],
-    data: { headerTitle: 'Symphony' },
+    data: { headerTitle: 'Symphony' }
   },
   {
     path: 'report',
-    loadChildren: () => import('./report/calculation-report.module').then(mod =>
-      mod.CalculationReportModule),
-    canActivate: [AuthenticationGuard],
+    // loadChildren: () => import('./report/calculation-report.module').then(mod =>
+    //   mod.CalculationReportModule),
+    children: [
+      {
+        path: 'compare/:aId/:bId',
+        component: ComparisonReportComponent
+      },
+      {
+        path: ':calcId',
+        component: CalculationReportComponent
+      }
+    ],
+    canActivate: [AuthenticationGuard]
     //data: { headerTitle: 'Symphony' }
   },
   { path: '', redirectTo: '/map', pathMatch: 'full' }
@@ -27,7 +41,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(
-      routes,
+      routes
       // { enableTracing: true } // <-- debugging purposes only
     )
   ],
