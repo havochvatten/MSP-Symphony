76e6d6359c25d95d4747af726de97b31ea8881e0
Ann <ann.ahlsten@havochvatten.se>
2022-11-21 18:36:18 +0100

    Differentiate build version with respect to WIO instance

--- frontend/src/build-info.ts
+++ frontend/src/build-info.ts
@@ -1,6 +1,6 @@
 // This file should be updated by CI system with actual build information
 export default {
-  version: '1.5.0',
+  version: '1.5.0-wio',
   buildNumber: '<unknown>',
   gitSha: '82daf28' // Deeplink to source repo?
 };
