3b8e55fb3ea6cd0e664d93fa7fcc84eb4f7a8243
Ann <ann.ahlsten@havochvatten.se>
2022-11-24 07:51:15 +0100

    Sparse object mapping for list of comparable scenarios

--- symphony-ws/src/main/java/se/havochvatten/symphony/dto/CalculationResultSlice.java
+++ symphony-ws/src/main/java/se/havochvatten/symphony/dto/CalculationResultSlice.java
@@ -1,17 +1,37 @@
 package se.havochvatten.symphony.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.geotools.data.geojson.GeoJSONReader;
+import org.opengis.feature.simple.SimpleFeature;
 import se.havochvatten.symphony.entity.CalculationResult;
 
+import java.io.IOException;
 import java.util.Date;
 
 // TODO: Do away with this one? Or at least include parameters
 // (when ROI has been replaced with an ID instead of the full polygon data)
 public class CalculationResultSlice {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    private String featureJson;
+
     public int id;
     public String name; // params.areaName by default
     public long timestamp;
+    @JsonIgnore
+    public SimpleFeature getFeature() {
+        if(featureJson == null)
+            return null;
+
+        try {
+            return GeoJSONReader.parseFeature(featureJson);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to parse feature: "+featureJson);
+        }
+    };
     // add optional param?
 
     public CalculationResultSlice(CalculationResult res) {
@@ -26,6 +46,14 @@ public class CalculationResultSlice {
         this.id = id;
         this.name = name;
         this.timestamp = timestamp.getTime();
+    }
+
+    @JsonCreator
+    public CalculationResultSlice(int id, String name, Date timestamp, String feature) {
+        this.id = id;
+        this.name = name;
+        this.timestamp = timestamp.getTime();
+        this.featureJson = feature;
     }
 
     public String getName() {return name;}
