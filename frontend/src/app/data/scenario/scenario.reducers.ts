havochvatten/MSP-Symphony

commit 112d5a31ff6182fde907796cf6ab7fe91da7124e

fixed all type errors in code

frontend/src/app/data/scenario/scenario.reducers.ts
@@ -1,12 +1,16 @@
-import { createReducer, on } from "@ngrx/store";
-import { ScenarioActions, ScenarioInterfaces } from "@data/scenario/index";
-import { CalculationActions } from "@data/calculation";
-import { removeIn, setIn, updateIn } from "immutable";
-import { Feature, GeoJsonProperties } from "geojson";
-import { AreaActions} from "@data/area";
-import { Polygon } from "@data/area/area.interfaces";
-import { fetchAreaMatrices, fetchAreaMatricesFailure, fetchAreaMatricesSuccess } from "@data/scenario/scenario.actions";
-import { isEqual } from "lodash";
+import { createReducer, on } from '@ngrx/store';
+import { ScenarioActions, ScenarioInterfaces } from '@data/scenario/index';
+import { CalculationActions } from '@data/calculation';
+import { removeIn, setIn, updateIn } from 'immutable';
+import { Feature, GeoJsonProperties } from 'geojson';
+import { AreaActions } from '@data/area';
+import { Polygon } from '@data/area/area.interfaces';
+import {
+  fetchAreaMatrices,
+  fetchAreaMatricesFailure,
+  fetchAreaMatricesSuccess
+} from '@data/scenario/scenario.actions';
+import { isEqual } from 'lodash';
 
 export const initialState: ScenarioInterfaces.State = {
   scenarios: [],
@@ -24,10 +28,14 @@ export const scenarioReducer = createReducer(
   on(ScenarioActions.openScenario, (state, { scenario, index }) => ({
     ...state,
     active: index,
-    scenarios: updateIn(state.scenarios, [index, 'changes', 'features'], (features: Feature[]) =>
-      features.map(feature => setIn(feature, ['properties', 'visible'], true)))
+    scenarios: updateIn(state.scenarios, [index, 'changes', 'features'], features => {
+      const feat = Object(features);
+      if (Symbol.iterator in feat) {
+        feat.map((feature: unknown) => setIn(feature, ['properties', 'visible'], true));
+      }
+    })
   })),
-  on(ScenarioActions.closeActiveScenario, (state) => ({
+  on(ScenarioActions.closeActiveScenario, state => ({
     ...state,
     active: undefined,
     activeFeature: undefined
@@ -35,7 +43,7 @@ export const scenarioReducer = createReducer(
   on(ScenarioActions.addScenario, (state, { scenario }) => ({
     ...state,
     scenarios: [scenario, ...state.scenarios], // prepend, since it will be newest
-    active: 0,
+    active: 0
   })),
   on(ScenarioActions.deleteScenario, (state, _) => ({
     ...state,
@@ -57,81 +65,122 @@ export const scenarioReducer = createReducer(
     scenarios: setIn(state.scenarios, [state.active, attribute], value)
   })),
   on(AreaActions.updateSelectedArea, (state, { statePath }) => {
-    if (state.active === undefined)
-      return state;
+    if (state.active === undefined) return state;
     else {
       const featureIndex = state.scenarios[state.active].changes.features.findIndex((f: Feature) =>
-        isEqual(f.properties!.statePath, statePath)); // or just check id?
+        isEqual(f.properties!.statePath, statePath)
+      ); // or just check id?
       return {
         ...state,
         activeFeature: featureIndex !== -1 ? featureIndex : undefined
-      }
+      };
     }
   }),
-  on(ScenarioActions.updateBandAttribute, (state, { area, componentType, bandId, band, attribute, value }) => {
-    const featureIndex = state.activeFeature ?? // activeFeature is wrong!
-      state.scenarios[state.active!].changes.features.findIndex((f: Feature) =>
-          f!.id == area.feature.properties.id);
+  on(
+    ScenarioActions.updateBandAttribute,
+    (state, { area, componentType, bandId, band, attribute, value }) => {
+      const featureIndex =
+        state.activeFeature ?? // activeFeature is wrong!
+        state.scenarios[state.active!].changes.features.findIndex(
+          (f: Feature) => f!.id == area.feature.properties.id
+        );
 
-    if (featureIndex == -1) {
-      const isWholeScenarioFeature = area.feature.properties.id == state.scenarios[state.active!].feature.properties?.id;
-      const featureToBeAdded = createGeoJSONFeature(area.polygon, area.feature.properties.id, {
-        title: area?.name,
-        visible: true,
-        displayName: area?.displayName,
-        statePath: area.statePath, // perhaps useful
-        changes: {
-          [bandId]: {
-            type: componentType,
-            band: band,
-            [attribute]: value
+      if (featureIndex == -1) {
+        const isWholeScenarioFeature =
+          area.feature.properties.id == state.scenarios[state.active!].feature.properties?.id;
+        const featureToBeAdded = createGeoJSONFeature(area.polygon, area.feature.properties.id, {
+          title: area?.name,
+          visible: true,
+          displayName: area?.displayName,
+          statePath: area.statePath, // perhaps useful
+          changes: {
+            [bandId]: {
+              type: componentType,
+              band: band,
+              [attribute]: value
+            }
           }
-        }
-      });
+        });
 
-      return {
-        ...state,
-        scenarios: updateIn(state.scenarios, [state.active, 'changes', 'features'],
-          features => isWholeScenarioFeature ?
-            [featureToBeAdded, ...features] :
-            [...features, featureToBeAdded]),
-        activeFeature: isWholeScenarioFeature ? 0 : state.scenarios[state.active!].changes.features.length-1+1
-      }
+        return {
+          ...state,
+          scenarios: updateIn(
+            state.scenarios,
+            [state.active, 'changes', 'features'],
+            (features: unknown) => {
+              // check for iterable: change uknown to object
+              // then if it has Symbol.iterator, it is type of iterable
+              // so spread operator (...) can be used
+              const feat = Object(features);
+              if (Symbol.iterator in Object(features)) {
+                isWholeScenarioFeature ? [featureToBeAdded, ...feat] : [...feat, featureToBeAdded];
+              }
+            }
+          ),
+          activeFeature: isWholeScenarioFeature
+            ? 0
+            : state.scenarios[state.active!].changes.features.length - 1 + 1
+        };
+      } else
+        return {
+          ...state,
+          // TODO delete if all area changes are neutral?
+          scenarios: updateIn(
+            state.scenarios,
+            [state.active, 'changes', 'features', featureIndex, 'properties', 'changes', bandId],
+            change => {
+              if (typeof change === 'object') {
+                return {
+                  change,
+                  type: componentType,
+                  band,
+                  [attribute]: value
+                };
+              } else {
+                return {
+                  change,
+                  type: componentType,
+                  band,
+                  [attribute]: value
+                };
+              }
+            }
+          ),
+          activeFeature: featureIndex
+        };
     }
-    else
-      return {
-      ...state,
-        // TODO delete if all area changes are neutral?
-        scenarios: updateIn(state.scenarios, [state.active, 'changes', 'features', featureIndex,
-          'properties', 'changes', bandId], change => ({
-          ...change,
-          type: componentType,
-          band: band,
-          [attribute]: value
-        })),
-        activeFeature: featureIndex
-      }
-  }),
+  ),
   on(ScenarioActions.deleteBandChangeAttribute, (state, { featureIndex, bandId }) => ({
     ...state,
-    scenarios: removeIn(state.scenarios, [state.active, 'changes', 'features', featureIndex, 'properties', 'changes', bandId])
+    scenarios: removeIn(state.scenarios, [
+      state.active,
+      'changes',
+      'features',
+      featureIndex,
+      'properties',
+      'changes',
+      bandId
+    ])
   })),
   on(ScenarioActions.setChangeAreaVisibility, (state, { featureIndex, visible }) => ({
     ...state,
-    scenarios: setIn(state.scenarios, [state.active, 'changes', 'features', featureIndex,
-      'properties', 'visible'], visible)
+    scenarios: setIn(
+      state.scenarios,
+      [state.active, 'changes', 'features', featureIndex, 'properties', 'visible'],
+      visible
+    )
   })),
-  on(ScenarioActions.hideAllChangeAreas, (state, {  }) => ({
+  on(ScenarioActions.hideAllChangeAreas, (state, {}) => ({
     ...state,
-    scenarios: updateIn(state.scenarios, [state.active, 'changes', 'features'], (features: Feature[]) =>
-      features.map(feature => setIn(feature, ['properties', 'visible'], false))
+    scenarios: updateIn(state.scenarios, [state.active, 'changes', 'features'], features =>
+      (features as Feature[]).map(feature => setIn(feature, ['properties', 'visible'], false))
     )
   })),
   on(ScenarioActions.deleteChangeFeature, (state, { featureIndex }) => ({
     ...state,
     scenarios: removeIn(state.scenarios, [state.active, 'changes', 'features', featureIndex])
   })),
-  on(fetchAreaMatrices, (state, {geometry}) => ({
+  on(fetchAreaMatrices, (state, { geometry }) => ({
     ...state,
     matricesLoading: true
   })),
@@ -139,15 +188,19 @@ export const scenarioReducer = createReducer(
     ...state,
     matricesLoading: false
   })),
-  on(fetchAreaMatricesFailure, state  => ({
+  on(fetchAreaMatricesFailure, state => ({
     ...state,
     matricesLoading: false
-  })),
+  }))
 );
 
-function createGeoJSONFeature(geometry: Polygon, id: string | number, properties: GeoJsonProperties) {
+function createGeoJSONFeature(
+  geometry: Polygon,
+  id: string | number,
+  properties: GeoJsonProperties
+) {
   return {
-    type: "Feature",
+    type: 'Feature',
     geometry,
     id,
     properties

frontend/src/app/data/area/area.reducers.ts
@@ -1,7 +1,7 @@
 import { createReducer, on } from '@ngrx/store';
 import { AreaActions, AreaInterfaces } from './';
 import { getIn, setIn, updateIn } from 'immutable';
-import { ScenarioActions } from "@data/scenario";
+import { ScenarioActions } from '@data/scenario';
 
 export const initialState: AreaInterfaces.State = {
   areaTypes: [],
@@ -27,13 +27,13 @@ export const areaReducer = createReducer(
   })),
   on(AreaActions.updateSelectedArea, (state, { statePath }) => ({
     ...state,
-    currentSelection: statePath//feature?.get("statePath")
+    currentSelection: statePath //feature?.get("statePath")
   })),
   // on(ScenarioActions.openScenario, (state, { scenario, index }) => ({
   //   ...state,
   //   currentSelection: scenario.feature?.properties?.['statePath']
   // })),
-  on(ScenarioActions.closeActiveScenario, (state) => ({
+  on(ScenarioActions.closeActiveScenario, state => ({
     ...state,
     currentSelection: undefined
   })),
@@ -76,11 +76,14 @@ export const areaReducer = createReducer(
   })),
   on(ScenarioActions.fetchAreaMatricesSuccess, (state, { matrixData }) => ({
     ...state,
-    selectionMatrices: matrixData,
+    selectionMatrices: matrixData
   })),
   on(AreaActions.addUserDefinedMatrix, (state, { matrix }) =>
-      updateIn(state, ['selectionMatrices', 'defaultArea', 'userDefinedMatrices'],
-        oldMatrices => oldMatrices.concat(matrix) // sort?
-      )
+    updateIn(state, ['selectionMatrices', 'defaultArea', 'userDefinedMatrices'], oldMatrices => {
+      const mat = Object(oldMatrices);
+      if (Symbol.iterator in mat) {
+        return mat.concat(matrix); // sort?
+      }
+    })
   )
 );

frontend/src/app/data/area/area.selectors.ts
@@ -13,8 +13,8 @@ import {
   Boundary
 } from './area.interfaces';
 import { getIn } from 'immutable';
-import { selectBaseline } from '@data/user/user.selectors';
-import { Baseline } from '@data/user/user.interfaces';
+// import { selectBaseline } from '@data/user/user.selectors';
+// import { Baseline } from '@data/user/user.interfaces';
 
 export const selectAreaState = createFeatureSelector<AppState, State>('area');
 
@@ -27,41 +27,35 @@ export const selectSelectedArea = createSelector(
 export const selectSelectedAreaData = createSelector(
   selectAreaState,
   selectSelectedArea,
-  (area: State, selectedArea) => selectedArea ? getIn(area, selectedArea, undefined) : undefined
+  (area: State, selectedArea) => (selectedArea ? getIn(area, selectedArea, undefined) : undefined)
 );
 
-export const selectSelectedAreaDataAndBaseline = createSelector(
-  selectSelectedAreaData,
-  selectBaseline,
-  (area?: Area, baseline?: Baseline) => ({ area, baseline })
+// Not used anywhere.. also problem with type of the selectSelectedAreaData
+// export const selectSelectedAreaDataAndBaseline = createSelector(
+//   selectSelectedAreaData,
+//   selectBaseline,
+//   (area?: Area, baseline?: Baseline) => ({ area, baseline })
+// );
+
+export const selectNationalAreas = createSelector(selectAreaState, (state: State) => {
+  const { area } = state;
+  const areaTypes = state.areaTypes.filter(areaType => Object.keys(area).includes(areaType));
+  return areaTypes
+    .map(areaType => area[areaType])
+    .map(nationalArea => ({
+      ...nationalArea,
+      groups: Object.values(nationalArea.groups).map(group => ({
+        ...group,
+        areas: Object.values(group.areas)
+      }))
+    }));
+});
+
+export const selectUserAreas = createSelector(selectAreaState, (state: State) =>
+  Object.values(state.userArea)
 );
 
-export const selectNationalAreas = createSelector(
-  selectAreaState,
-  (state: State) => {
-    const { area } = state;
-    const areaTypes = state.areaTypes.filter(areaType => Object.keys(area).includes(areaType));
-    return areaTypes
-      .map(areaType => area[areaType])
-      .map(nationalArea => ({
-        ...nationalArea,
-        groups: Object.values(nationalArea.groups).map(group => ({
-          ...group,
-          areas: Object.values(group.areas)
-        }))
-      }));
-  }
-);
-
-export const selectUserAreas = createSelector(
-  selectAreaState,
-  (state: State) => Object.values(state.userArea)
-);
-
-export const selectBoundaries = createSelector(
-  selectAreaState,
-  state => state.boundaries
-);
+export const selectBoundaries = createSelector(selectAreaState, state => state.boundaries);
 
 export const selectAreaMatrixData = createSelector(
   selectAreaState,

frontend/src/app/data/metadata/metadata.effects.ts
@@ -4,32 +4,42 @@ import { catchError, concatMap, debounceTime, map, mergeMap, withLatestFrom } fr
 import { of } from 'rxjs';
 import MetadataService from './metadata.service';
 import { MetadataActions, MetadataSelectors } from './';
-import { APILayerData, Band, BandGroup, ComponentKey, Components, Groups } from './metadata.interfaces';
+import {
+  APILayerData,
+  Band,
+  BandGroup,
+  ComponentKey,
+  Components,
+  Groups
+} from './metadata.interfaces';
 import { Store } from '@ngrx/store';
 import { State } from '@src/app/app-reducer';
-import { getIn } from "immutable";
-import { ScenarioActions } from "@data/scenario";
-import { getComponentType } from "@data/metadata/metadata.selectors";
-import { findBestLanguageMatch } from "@src/app/app-translation-setup.module";
+import { getIn } from 'immutable';
+import { ScenarioActions } from '@data/scenario';
+import { getComponentType } from '@data/metadata/metadata.selectors';
+import { findBestLanguageMatch } from '@src/app/app-translation-setup.module';
 
 @Injectable()
 export class MetadataEffects {
   constructor(
     private actions$: Actions,
     private store: Store<State>,
-    private metadataService: MetadataService,
+    private metadataService: MetadataService
   ) {}
 
   @Effect()
   fetchMetadata$ = this.actions$.pipe(
     ofType(MetadataActions.fetchMetadata),
-    mergeMap(({baseline}) =>
+    mergeMap(({ baseline }) =>
       this.metadataService.getMetaData(baseline).pipe(
         map(layerData => {
           const newLayerData = {
             ...layerData,
             ecoComponent: this.formatComponentData(layerData, 'ecoComponent'),
-            pressureComponent: this.formatComponentData(layerData, 'pressureComponent'/*, language*/)
+            pressureComponent: this.formatComponentData(
+              layerData,
+              'pressureComponent' /*, language*/
+            )
           };
           return MetadataActions.fetchMetadataSuccess({ metadata: newLayerData });
         }),
@@ -51,25 +61,30 @@ export class MetadataEffects {
   updateMultiplierMapState$ = this.actions$.pipe(
     ofType(MetadataActions.updateMultiplier),
     debounceTime(200),
-    concatMap(action => of(action).pipe(
-      withLatestFrom(this.store.select(MetadataSelectors.selectMetadataState)), // for NgRx 11+, use concatLatestFrom
-    )),
-    map(([{ area, bandPath, value }, metadata]) =>
+    concatMap(action =>
+      of(action).pipe(
+        withLatestFrom(this.store.select(MetadataSelectors.selectMetadataState)) // for NgRx 11+, use concatLatestFrom
+      )
+    ),
+    map(([{ area, bandPath, value }, metadata]) => {
       // Here we could fetch the state from the actual map feature using a Promise or such
+      let getBand = getIn(metadata, [...bandPath, 'bandNumber'], NaN);
+      if (typeof getBand !== 'number') {
+        return;
+      }
+
       ScenarioActions.updateBandAttribute({
         area, // FIXME make sure we can only change when selected
         componentType: getComponentType(bandPath),
-        bandId: bandPath[bandPath.length-1] as string,
-        band: getIn(metadata, [...bandPath, 'bandNumber'], NaN),
+        bandId: bandPath[bandPath.length - 1] as string,
+        band: getBand,
         attribute: 'multiplier',
-        value,
-      }))
+        value
+      });
+    })
   );
 
-  private formatComponentData(
-    layerData: APILayerData,
-    componentType: ComponentKey,
-  ): Groups {
+  private formatComponentData(layerData: APILayerData, componentType: ComponentKey): Groups {
     const useMetadataLocalLang = findBestLanguageMatch([layerData.language]);
     return layerData[componentType].symphonyTeams.reduce((teams: Groups, team: BandGroup) => {
       teams[team.symphonyTeamName] = {

frontend/src/app/data/scenario/scenario.effects.ts
@@ -1,29 +1,36 @@
-import { Injectable } from "@angular/core";
-import { catchError, concatMap, filter, map, mergeMap, retry, withLatestFrom } from "rxjs/operators";
-import { Actions, Effect, ofType } from "@ngrx/effects";
-import { ScenarioService } from "@data/scenario/scenario.service";
-import { ScenarioActions, ScenarioSelectors } from "@data/scenario/index";
-import { of } from "rxjs";
-import { Store } from "@ngrx/store";
-import { State } from "@src/app/app-reducer";
-import { getIn, removeIn, setIn, updateIn } from "immutable";
-import { UserSelectors } from "@data/user";
+import { Injectable } from '@angular/core';
 import {
-  AreaMatrixData
-} from "@src/app/map-view/scenario/scenario-detail/matrix-selection/matrix.interfaces";
+  catchError,
+  concatMap,
+  filter,
+  map,
+  mergeMap,
+  retry,
+  withLatestFrom
+} from 'rxjs/operators';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { ScenarioService } from '@data/scenario/scenario.service';
+import { ScenarioActions, ScenarioSelectors } from '@data/scenario/index';
+import { of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { State } from '@src/app/app-reducer';
+import { getIn, removeIn, setIn, updateIn } from 'immutable';
+import { UserSelectors } from '@data/user';
+import { AreaMatrixData } from '@src/app/map-view/scenario/scenario-detail/matrix-selection/matrix.interfaces';
 import {
   fetchAreaMatrices,
   fetchAreaMatricesFailure,
   fetchAreaMatricesSuccess
-} from "@data/scenario/scenario.actions";
-import { CalculationActions } from "@data/calculation";
-import { Feature } from "geojson";
+} from '@data/scenario/scenario.actions';
+import { CalculationActions } from '@data/calculation';
+import { Feature } from 'geojson';
 
 @Injectable()
 export class ScenarioEffects {
-  constructor(private actions$: Actions,
-              private store: Store<State>,
-              private scenarioService: ScenarioService
+  constructor(
+    private actions$: Actions,
+    private store: Store<State>,
+    private scenarioService: ScenarioService
   ) {}
 
   @Effect()
@@ -49,36 +56,44 @@ export class ScenarioEffects {
         catchError(({ status, error: message }) =>
           of(ScenarioActions.saveScenarioFailure({ error: { status, message } }))
         )
-      )
-    }
-    )
+      );
+    })
   );
 
   @Effect()
   deleteScenario$ = this.actions$.pipe(
     ofType(ScenarioActions.deleteScenario),
     mergeMap(({ scenarioToBeDeleted }) => {
-        return this.scenarioService.delete(scenarioToBeDeleted.id).pipe(
-          map(() => ScenarioActions.deleteScenarioSuccess()),
-          catchError(({ status, error: message }) =>
-            of(ScenarioActions.deleteScenarioFailure({ error: { status, message } }))
-          )
+      return this.scenarioService.delete(scenarioToBeDeleted.id).pipe(
+        map(() => ScenarioActions.deleteScenarioSuccess()),
+        catchError(({ status, error: message }) =>
+          of(ScenarioActions.deleteScenarioFailure({ error: { status, message } }))
         )
-      }
-    )
+      );
+    })
   );
 
   @Effect()
   removeFeature$ = this.actions$.pipe(
     ofType(ScenarioActions.deleteBandChangeOrChangeFeature),
-    concatMap(action => of(action).pipe(
-      withLatestFrom(this.store.select(ScenarioSelectors.selectActiveScenarioChangeFeatures))
-    )),
+    concatMap(action =>
+      of(action).pipe(
+        withLatestFrom(this.store.select(ScenarioSelectors.selectActiveScenarioChangeFeatures))
+      )
+    ),
     map(([{ featureIndex, bandId }, features]) => {
-      const featuresWithoutChangeAttribute = removeIn(features, [featureIndex, 'properties', 'changes', bandId]);
-      const doesFeatureStillHasChanges = Object.keys(
-          getIn(featuresWithoutChangeAttribute, [featureIndex, 'properties', 'changes'], {})
-        ).length>0;
+      const featuresWithoutChangeAttribute = removeIn(features, [
+        featureIndex,
+        'properties',
+        'changes',
+        bandId
+      ]);
+
+      const getObject = Object(
+        getIn(featuresWithoutChangeAttribute, [featureIndex, 'properties', 'changes'], {})
+      );
+
+      const doesFeatureStillHasChanges = getObject && Object.keys(getObject).length > 0;
       if (doesFeatureStillHasChanges)
         return ScenarioActions.deleteBandChangeAttribute({ featureIndex, bandId });
       else {
@@ -93,7 +108,7 @@ export class ScenarioEffects {
   toggleChangeVisibility$ = this.actions$.pipe(
     ofType(ScenarioActions.toggleChangeAreaVisibility),
     map(({ feature, featureIndex }) => {
-      const visible = this.scenarioService.setScenarioChangeVisibility(feature);
+      const visible = !!this.scenarioService.setScenarioChangeVisibility(feature);
       return ScenarioActions.setChangeAreaVisibility({ featureIndex, visible });
     })
   );
@@ -107,24 +122,20 @@ export class ScenarioEffects {
     })
   );
 
-
   @Effect()
   fetchMatrices$ = this.actions$.pipe(
     ofType(fetchAreaMatrices),
-    concatMap(action => of(action).pipe(
-      withLatestFrom(this.store.select(UserSelectors.selectBaseline))
-    )),
+    concatMap(action =>
+      of(action).pipe(withLatestFrom(this.store.select(UserSelectors.selectBaseline)))
+    ),
     filter(([_, baseline]) => baseline !== undefined),
-    mergeMap(([{geometry}, baseline]) =>
-      this.scenarioService.getAreaMatrixParams(geometry, baseline!.name)
-        .pipe(
-          map((matrixData: AreaMatrixData) =>
-            fetchAreaMatricesSuccess({matrixData}),
-          ),
-          catchError(({ status, error }) =>
-            of(fetchAreaMatricesFailure({ error: { status, message: error.errorMessage } }))
-          )
+    mergeMap(([{ geometry }, baseline]) =>
+      this.scenarioService.getAreaMatrixParams(geometry, baseline!.name).pipe(
+        map((matrixData: AreaMatrixData) => fetchAreaMatricesSuccess({ matrixData })),
+        catchError(({ status, error }) =>
+          of(fetchAreaMatricesFailure({ error: { status, message: error.errorMessage } }))
         )
+      )
     )
   );
 }

frontend/src/app/map-view/band-selection/band-selection.component.ts
@@ -6,6 +6,7 @@ import { MetadataActions } from '@data/metadata';
 import { Observable } from 'rxjs';
 import { AreaSelectors } from '@data/area';
 import { Area } from '@data/area/area.interfaces';
+import { map } from 'lodash';
 
 //=== SEARCH UTILITIES ===
 
@@ -53,7 +54,7 @@ export class BandSelectionComponent implements OnInit, OnChanges {
   @Input() bandType: BandType = 'ecoComponents';
   search = '';
   filteredGroups: BandGroup[] = [];
-  selectedArea?: Observable<Area | undefined>;
+  selectedArea?: Observable<Area | undefined | unknown>;
 
   constructor(private store: Store<State>) {}
 

commit 2ec6c1c494c1a8e95490ce7b3998364b81567e12

Fixes error with scenario change serialization
Error introduced by commit 112d5a3

frontend/src/app/data/scenario/scenario.reducers.ts
@@ -116,14 +116,11 @@ export const scenarioReducer = createReducer(
             updateIn(
             state.scenarios,
             [state.active, 'changes', 'features', featureIndex, 'properties', 'changes', bandId],
-            change => {
-              return {
-                  change,
+            change => ({
                   type: componentType,
                   band,
                   [attribute]: value
-                };
-            }) : state.scenarios,
+                })) : state.scenarios,
           activeFeature: featureIndex
         };
     }