2350d667706705e146b8c1b7a877ae0f298e2571
Ann <ann.ahlsten@havochvatten.se>
2023-06-28 12:56:33 +0200

    Basic unit test case for TransferChangesComponent

--- frontend/src/app/map-view/scenario/transfer-changes/transfer-changes.component.spec.ts
+++ frontend/src/app/map-view/scenario/transfer-changes/transfer-changes.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { StoreModule } from "@ngrx/store";
+import { DialogService } from "@shared/dialog/dialog.service";
+import { DialogRef } from "@shared/dialog/dialog-ref";
+import { DialogConfig } from '@shared/dialog/dialog-config';
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
 import { TransferChangesComponent } from './transfer-changes.component';
 
 describe('TransferChangesComponent', () => {
@@ -8,6 +13,25 @@ describe('TransferChangesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({}, {}),
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        DialogService,
+        DialogRef,
+        TranslateService,
+        {
+          provide: DialogConfig,
+          useValue: {
+            data: {
+              target: {
+                areas: []
+              }
+            }
+          }
+        }
+      ],
       declarations: [ TransferChangesComponent ]
     })
     .compileComponents();
