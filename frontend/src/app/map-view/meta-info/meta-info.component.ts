142ccff262bc8742e9186d9c7e4169c5a8816069
Ann <ann.ahlsten@havochvatten.se>
2023-08-01 13:51:39 +0200

    SYM-554: Process escaped newlines into paragraphs
    Applies to visible metadata fields.
    Following a suggestion by @EdSacre
    https://github.com/havochvatten/MSP-Symphony/discussions/107

--- frontend/src/app/map-view/meta-info/meta-info.component.ts
+++ frontend/src/app/map-view/meta-info/meta-info.component.ts
@@ -14,18 +14,18 @@ export class MetaInfoComponent implements AfterViewInit {
 
   private band: Band;
   public bandMetadataSources: string[];
-  public bandMetadata: Map<string, string>;
+  public bandMetadata: Map<string, string[]>;
   public category: string;
   public title: string;
 
   constructor(private dialog: DialogRef, private config: DialogConfig,
               private container: ElementRef) {
     this.band = config.data.band;
-    this.bandMetadata = new Map<string, string> ([
-      ['method-summary',     this.band.methodSummary],
-      ['known-limitations',  this.band.limitationsForSymphony],
-      ['value-range',        this.band.valueRange],
-      ['data-processing',    this.band.dataProcessing]
+    this.bandMetadata = new Map<string, string[]> ([
+      ['method-summary',     this.band.methodSummary?.split('\\n')],
+      ['known-limitations',  this.band.limitationsForSymphony?.split('\\n')],
+      ['value-range',        this.band.valueRange?.split('\\n')],
+      ['data-processing',    this.band.dataProcessing?.split('\\n')],
     ]);
     this.bandMetadataSources = this.band.dataSources?.split(';') || [];
     this.category = this.band.statePath[0] === 'ecoComponent' ? 'ecosystem' : 'pressure';
@@ -36,7 +36,7 @@ export class MetaInfoComponent implements AfterViewInit {
     this.dialog.close();
   }
 
-  preserveKV(a: KeyValue<string, string>, b: KeyValue<string, string>): number { return 0; }
+  preserveKV(a: KeyValue<string, string[]>, b: KeyValue<string, string[]>): number { return 0; }
 
   ngAfterViewInit(): void {
     if(this.container.nativeElement.scrollHeight > this.container.nativeElement.offsetHeight) {
