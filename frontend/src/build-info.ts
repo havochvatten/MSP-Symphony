c10f9fda94f5d47d398f7e700b5a577e7d2309d5
Ann Ahlsten <ann.ahlsten@havochvatten.se>
2024-01-18 16:27:22 +0100

    Dev cycle version iteration > 1.17

--- frontend/src/build-info.ts
+++ frontend/src/build-info.ts
@@ -1,6 +1,6 @@
 // This file should be updated by CI system with actual build information
 export default {
-  version: '1.16.0',
+  version: '1.17.0-SNAPSHOT',
   buildNumber: '<unknown>',
-  gitSha: '2736b0d' // Deeplink to source repo?
+  gitSha: '<unknown>' // Deeplink to source repo?
 };
