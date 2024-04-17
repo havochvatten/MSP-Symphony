eb114647801ed0d0d60b58f0d8c7765686757024
Ann <ann.ahlsten@havochvatten.se>
2024-04-17 09:03:22 +0200

    Snapshot version (amend b7111ee)

--- frontend/src/build-info.ts
+++ frontend/src/build-info.ts
@@ -1,6 +1,6 @@
 // This file should be updated by CI system with actual build information
 export default {
-  version: '1.21.0',
+  version: '1.21.0-SNAPSHOT',
   buildNumber: '<unknown>',
   gitSha: '<unknown>' // Deeplink to source repo?
 };
