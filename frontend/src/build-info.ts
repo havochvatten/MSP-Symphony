4837e8c63ef946d776f804f7725590f07bfed39c
Ann <ann.ahlsten@havochvatten.se>
2024-10-24 10:13:04 +0200

    Patch release 1.22.1

--- frontend/src/build-info.ts
+++ frontend/src/build-info.ts
@@ -1,6 +1,6 @@
 // This file should be updated by CI system with actual build information
 export default {
-  version: '1.22.0',
+  version: '1.22.1',
   buildNumber: '<unknown>',
-  gitSha: 'a994f05' // Deeplink to source repo?
+  gitSha: '2eadc6d' // Deeplink to source repo?
 };
