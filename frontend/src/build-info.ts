b5bc6a4eb2fd3324065c37399a4335049550486d
Ann <ann.ahlsten@havochvatten.se>
2025-06-13 13:04:56 +0200

    Dev cycle version iteration > 1.24

--- frontend/src/build-info.ts
+++ frontend/src/build-info.ts
@@ -1,6 +1,6 @@
 // This file should be updated by CI system with actual build information
 export default {
-  version: '1.23.0',
+  version: '1.24.0-SNAPSHOT',
   buildNumber: '<unknown>',
-  gitSha: '28cb302' // Deeplink to source repo?
+  gitSha: '<unknown>' // Deeplink to source repo?
 };
