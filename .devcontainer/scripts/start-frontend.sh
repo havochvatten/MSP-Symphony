d226ad14da152ef236c772257f2b81bdb5e96e96
DavidGidlof <117651607+Ladibo@users.noreply.github.com>
2026-02-16 13:29:17 +0000

    Initial changes to enable the application to run. Yarn removed from dockerfile and a small change in start-frontend.sh script

--- .devcontainer/scripts/start-frontend.sh
+++ .devcontainer/scripts/start-frontend.sh
@@ -7,7 +7,7 @@ KEY="/workspace/frontend/ssl/server.key"
 # Check if certificate and key exist
 if [ ! -f "$CERT" ] || [ ! -f "$KEY" ]; then
   echo "SSL certificate or key not found. Generating..."
-  npm run generate-cert
+  cd /workspace/frontend && npm run generate-cert
 fi
 
 cd /workspace/frontend && PROXY_TARGET=local ng serve --ssl --host 0.0.0.0
