c404e77980a90574495f7429223bf5548e70f6c9
Ann <ann.ahlsten@havochvatten.se>
2026-02-27 16:44:16 +0100

    Release 1.24.0

--- frontend/src/build-info.ts
+++ frontend/src/build-info.ts
@@ -1,6 +1,6 @@
 // This file should be updated by CI system with actual build information
 export default {
-  version: '1.24.0-SNAPSHOT',
+  version: '1.24.0',
   buildNumber: '<unknown>',
-  gitSha: '<unknown>' // Deeplink to source repo?
+  gitSha: '4048ffe' // Deeplink to source repo?
 };
