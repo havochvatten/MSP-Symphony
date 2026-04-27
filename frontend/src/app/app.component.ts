a7292bd785d7aa8047b80b262cb0d1edb8dcec13
Sebastian Odbjer <sebastian.odbjer@havochvatten.se>
2026-04-27 15:58:06 +0200

    [SYM-728] - Trigger login/resource loading spinner on page reload
    Previously it only triggered during the loging flow.
    Which meant that if you clear parts of your chache or reload the page,
    the resources will still get reloaded but without showing the spinner.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnDestroy, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Event, NavigationEnd, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
 import { filter, map, mergeMap } from 'rxjs/operators';
+
+import { State } from './app-reducer';
+import { UserSelectors } from '@data/user';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +16,15 @@ import { filter, map, mergeMap } from 'rxjs/operators';
 export class AppComponent implements OnDestroy {
   private readonly router = inject(Router);
   private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly store = inject<Store<State>>(Store);
 
   headerTitle = '';
+  isInitialLoading$: Observable<boolean>;
   routeDataSubscription: Subscription | undefined;
 
   constructor() {
+    this.isInitialLoading$ = this.store.select(UserSelectors.selectIsInitialLoading);
+
     this.routeDataSubscription = this.router.events
       .pipe(
         filter((e: Event) => e instanceof NavigationEnd),
@@ -27,12 +35,13 @@ export class AppComponent implements OnDestroy {
           }
           return route;
         }),
-        mergeMap(route => route.data)
+        mergeMap((route) => route.data)
       )
-      .subscribe(data => {
+      .subscribe((data) => {
         this.headerTitle = data['headerTitle'] ? data['headerTitle'] : '';
       });
   }
+
   ngOnDestroy() {
     if (this.routeDataSubscription) {
       this.routeDataSubscription.unsubscribe();
