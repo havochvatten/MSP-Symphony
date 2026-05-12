96a067ee7ec17e8c0fb6972764441644d71ad4e2
Toni Karunaratne <toni.karunaratne@havochvatten.se>
2026-05-12 13:19:48 +0200

    Remove unused/duplicate classes from public-map

--- frontend/src/app/public-viewer/public-map/layers/symphony-layer.ts
+++
@@ -1,9 +0,0 @@
-import LayerGroup from "ol/layer/Group";
-
-export abstract class SymphonyLayerGroup extends LayerGroup {
-  public antialias = true;
-
-  public toggleImageSmoothing(aliasing: boolean) {
-    this.antialias = aliasing;
-  }
-}
