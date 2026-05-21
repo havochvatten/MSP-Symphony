493e20df1e30f656e8f48ee67b7e8d92910c1733
Toni Karunaratne <toni.karunaratne@havochvatten.se>
2026-05-21 08:38:22 +0200

    Add public endpoint for public access availability flag

---
+++ symphony-ws/src/main/java/se/havochvatten/symphony/web/SystemPropertiesREST.java
@@ -0,0 +1,34 @@
+package se.havochvatten.symphony.web;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.security.PermitAll;
+import jakarta.ejb.Stateless;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import se.havochvatten.symphony.dto.SystemPropertiesDto;
+import se.havochvatten.symphony.service.PropertiesService;
+
+@Stateless
+@Tag(name = "/systemproperties")
+@PermitAll
+@Path("systemproperties")
+public class SystemPropertiesREST {
+    @Inject
+    private PropertiesService props;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSystemProperties() throws Exception{
+        boolean publicAccess = props.getPropertyAsBool("symphony.public_access", false);
+        SystemPropertiesDto properties = new SystemPropertiesDto(publicAccess);
+        return Response.ok(properties).build();
+    }
+
+
+
+}
