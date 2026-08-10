e0940c941130dfd37f7d1b55bd52c4bec5a3a326
idaborck <idaborck3@gmail.com>
2026-08-10 13:57:47 +0000

    Remove devcontainer, ignore node_modules

--- .devcontainer/scripts/stop-wildfly.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Shutting down WildFly..."
-/opt/wildfly/bin/jboss-cli.sh --connect command=:shutdown
-
-echo "Removing deployments..."
-rm -rf /opt/wildfly/standalone/deployments/*.war
